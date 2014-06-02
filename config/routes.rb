1a29488a497d948f96e3fea00e629fe4695982d3
Takamasa Suzuki <webcrafts098@gmail.com>
2014-06-02 21:16:10 +0900

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 OkinawaRamenMap::Application.routes.draw do
+  root 'pages#index'
+  resource :pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

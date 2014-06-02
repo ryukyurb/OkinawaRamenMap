d8444307f25cb46892c69cc6c41c93591f027506
Takamasa Suzuki <webcrafts098@gmail.com>
2014-06-02 21:02:13 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OkinawaRamenMap::Application.config.secret_key_base = '5130b8af274893b29d4056ce370ae4649928e0bd33342cff94e83ba42f23aea5fb0dce7d4dd476a1fb07b4e7af94a954fda8bf672a8e7bb8f464a12d5f73f7ef'
